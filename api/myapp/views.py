f1e9bf9bab0cb0cb205885858bd0aed674084cb6
FehRoque <feliperamosroque@gmail.com>
2024-03-30 10:32:23 -0300

    inital views

--- api/myapp/views.py
+++ api/myapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import DataEntry
+from .serializers import DataEntrySerializer
 
-# Create your views here.
+class DataView(APIView):
+    def get(self):
+        queryset = DataEntry.objects.all()
+        serializer = DataEntrySerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = DataEntrySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'Uploaded successfully'}, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
