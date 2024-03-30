c1b28383017710e54486b73d2e3e0d56ac0e3728
FehRoque <feliperamosroque@gmail.com>
2024-03-30 09:38:31 -0300

    gvars

---
+++ gvars.py
@@ -0,0 +1,10 @@
+"""
+gvars.py - Module for managing global variables.
+"""
+import os
+
+project_folder = os.getcwd()
+resources_folder = fr"{project_folder}\resources"
+tests_folder = fr"{resources_folder}\tests"
+xlsx_test_path = fr"{tests_folder}\test.xlsx"
+modelo_xlsx_path = fr"{resources_folder}\Modelo_Dados.xlsx"
