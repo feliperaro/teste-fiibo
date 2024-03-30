30169e1a728e9f30857ceb90951d3c4198cc2519
FehRoque <feliperamosroque@gmail.com>
2024-03-30 10:32:52 -0300

    models.DataEntry

--- api/myapp/models.py
+++ api/myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class DataEntry(models.Model):
+    notas = models.CharField(max_length=255)
+    competencia = models.CharField(max_length=255)
+    status = models.CharField(max_length=255)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
