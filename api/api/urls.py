8f77f66be45a484f587439848c7967f50165841e
FehRoque <feliperamosroque@gmail.com>
2024-03-30 10:36:52 -0300

    url removed

--- api/api/urls.py
+++ api/api/urls.py
@@ -16,9 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from myapp.views import DataView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('data/', DataView.as_view(), name='data_endpoint'),
 ]
